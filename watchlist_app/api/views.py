ff5a4d5b6d0544ca0e19bcde1468725afab1403a
kei_kusanagi_99 <kei_kusanagi@outlook.com>
2022-10-05 12:12:24 -0500

    add ['POST'] metod

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -4,11 +4,21 @@ from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
-@api_view()
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
 @api_view()
 def movie_details(request, pk):
