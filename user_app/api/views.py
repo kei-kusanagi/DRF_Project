5ef1dbff5958f748aeba3aab387e9dfde3c9c4c8
kei_kusanagi_99 <kei_kusanagi@outlook.com>
2022-11-08 15:55:01 -0600

    JWT Authentication - Registration

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -5,8 +5,10 @@ from rest_framework.authtoken.models import Token
 from rest_framework import status
 
 from user_app.api.serializers import RegistrationSerializer
-from user_app import models
+# from user_app import models
 
+# JWT Authentication - Registration
+from rest_framework_simplejwt.tokens import RefreshToken
 
 @api_view(['POST',])
 def logout_view(request):
@@ -31,8 +33,13 @@ def registration_view(request):
             data['username'] = account.username
             data['email'] = account.email
 
-            token = Token.objects.get(user=account).key
-            data['token'] = token
+            # token = Token.objects.get(user=account).key
+            # data['token'] = token
+            refresh = RefreshToken.for_user(account)
+            data['token'] = {
+                                'refresh': str(refresh),
+                                'access': str(refresh.access_token),
+                            }
         
         else:
             data = serializer.errors
--- watchmate/settings.py
+++ watchmate/settings.py
@@ -136,7 +136,11 @@ REST_FRAMEWORK = {
     ],
 }
 
+from datetime import timedelta
+
 SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS' : True,
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
 }
     
