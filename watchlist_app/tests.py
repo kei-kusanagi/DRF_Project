1af2f9dc4d3baf5f7555c320eb8d326b01588faf
kei_kusanagi_99 <kei_kusanagi@outlook.com>
2022-11-16 13:45:45 -0600

    API Testing - WatchList

--- watchlist_app/tests.py
+++ watchlist_app/tests.py
@@ -38,3 +38,44 @@ class StreamP1atformTestCase(APITestCase):
     def test_streamplataform_ind(self):
         response = self.client.get(reverse('streamplataform-detail', args=(self.stream.id,)))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class WatchListTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="example", password="Passwors@123")
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.stream = models.StreamPlataform.objects.create(
+            name= "Netflix",
+            about= "#1 Streaming Plataform",
+            website= "http://www.netflix.com",
+        )
+        
+        self.watchlist = models.WatchList.objects.create(
+            plataform=self.stream,
+            title= "Example Movie",
+            storyline="Example Story",
+            active= True
+        )
+
+    def test_watchlist_create(self):
+        data = {
+            "plataform": self.stream,
+            "title": "Example Movie",
+            "storyline": "Example Story",
+            "active": True
+        }
+        response = self.client.post(reverse('movie-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
+    def test_watchlist_list(self):
+        response = self.client.get(reverse('movie-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_watchlist_ind(self):
+        response = self.client.get(reverse('movie-detail', args=(self.watchlist.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(models.WatchList.objects.get().title, 'Example Movie')
+        self.assertEqual(models.WatchList.objects.count(), 1)
+        
