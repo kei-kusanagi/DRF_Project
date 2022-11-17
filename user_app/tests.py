8d7662db3f9f2257b87999556023a3372077ec43
kei_kusanagi_99 <kei_kusanagi@outlook.com>
2022-11-17 12:08:07 -0600

    API Testing - UserTestCase

--- user_app/tests.py
+++ user_app/tests.py
@@ -36,3 +36,7 @@ class RegisterTestCase(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
         response = self.client.post(reverse('logout'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_review_user(self):
+        response = self.client.get('/watch/reviews/?username' + self.user.username)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
